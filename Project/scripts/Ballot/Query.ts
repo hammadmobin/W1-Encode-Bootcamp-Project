ad505e1ffbe219f76d795a2c2562cd0b3518eb69
udibgui <70275038+udibgui@users.noreply.github.com>
2022-07-17 15:31:12 +0300

    Add Query.ts code

---
+++ Project/scripts/Ballot/Query.ts
@@ -0,0 +1,51 @@
+import { Contract, ethers } from "ethers";
+import "dotenv/config";
+import * as ballotJson from "../../artifacts/contracts/Ballot.sol/Ballot.json";
+import { Ballot } from "../../typechain";
+//import ballot from "../../registry.json";
+
+// This key is already public on Herong's Tutorial Examples - v1.03, by Dr. Herong Yang
+// Do never expose your keys like this
+const EXPOSED_KEY = "8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de8f";
+//const ballotAddress = ballot.ballotAddress;
+
+async function main() {
+  const wallet =
+    process.env.MNEMONIC && process.env.MNEMONIC.length > 0
+      ? ethers.Wallet.fromMnemonic(process.env.MNEMONIC)
+      : new ethers.Wallet(process.env.PRIVATE_KEY ?? EXPOSED_KEY);
+  console.log(`Using address ${wallet.address}`);
+  const provider = ethers.providers.getDefaultProvider("ropsten");
+  const signer = wallet.connect(provider);
+  const balanceBN = await signer.getBalance();
+  const balance = Number(ethers.utils.formatEther(balanceBN));
+  console.log(`Wallet balance ${balance}`);
+  if (balance < 0.01) {
+    throw new Error("Not enough ether");
+  }
+  if (process.argv.length < 3) throw new Error("Ballot address missing");
+  const ballotAddress = process.argv[2];
+  if (process.argv.length < 4) throw new Error("Voter address missing");
+  const voterAddress = process.argv[3];
+  
+  console.log(
+    `Attaching ballot contract interface to address ${ballotAddress}`
+  );
+  //Connect wallet to contract
+  const ballotContract: Ballot = new Contract(
+    ballotAddress,
+    ballotJson.abi,
+    signer
+  ) as Ballot;
+
+// Call the winnerName function from the ballot contract
+const winningProposalName = await ballotContract.winnerName();
+//Display the winning proposal. Make sure to convert bytes32 to string
+console.log("And the winning proposal is...: " + ethers.utils.parseBytes32String(winningProposalName));
+
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
