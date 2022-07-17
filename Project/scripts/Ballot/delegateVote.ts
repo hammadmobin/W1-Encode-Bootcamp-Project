e4e7b42bebe6854393285e6c770cbfa14fb02aaa
Muhammad Hammad Mobin <muhammadhammadmobin@gmail.com>
2022-07-17 13:14:20 +0500

    Work on the DelegateVote

--- Project/scripts/Ballot/delegateVote.ts
+++ Project/scripts/Ballot/delegateVote.ts
@@ -4,6 +4,7 @@ import * as ballotJson from "../../artifacts/contracts/Ballot.sol/Ballot.json";
 // eslint-disable-next-line node/no-missing-import
 import { Ballot } from "../../typechain";
 
+
 // This key is already public on Herong's Tutorial Examples - v1.03, by Dr. Herong Yang
 // Do never expose your keys like this
 const EXPOSED_KEY =
@@ -15,6 +16,7 @@ async function main() {
       ? ethers.Wallet.fromMnemonic(process.env.MNEMONIC)
       : new ethers.Wallet(process.env.PRIVATE_KEY ?? EXPOSED_KEY);
   console.log(`Using address ${wallet.address}`);
+
 
   const provider = ethers.providers.getDefaultProvider("ropsten");
 
@@ -29,6 +31,8 @@ async function main() {
   if (balance < 0.01) {
     throw new Error("Not enough ether");
   }
+
+
   if (process.argv.length < 3) throw new Error("Ballot address missing");
   const ballotAddress = process.argv[2];
   if (process.argv.length < 4) throw new Error("Voter address missing");
@@ -36,23 +40,35 @@ async function main() {
   console.log(
     `Attaching ballot contract interface to address ${ballotAddress}`
   );
-  
   const ballotContract: Ballot = new Contract(
     ballotAddress,
     ballotJson.abi,
     signer
   ) as Ballot;
-  const chairpersonAddress = await ballotContract.chairperson();
-  if (chairpersonAddress !== signer.address)
-    throw new Error("Caller is not the chairperson for this contract");
-  console.log(`Giving right to vote to ${voterAddress}`);
-  const tx = await ballotContract.giveRightToVote(voterAddress);
+    const va = await ballotContract.voters(wallet.address)
+    if(va.voted == true)
+    throw new Error("you already voted !");
+    
+    if (wallet.address == voterAddress)
+    throw new Error("Self-delegation is disallowed.");
+
+    const da = await ballotContract.voters(voterAddress)
+
+    if(da.voted == true)
+    throw new Error("Voter already voted ! ");
+
+
+  const tx = await ballotContract.delegate(voterAddress);
+
   console.log("Awaiting confirmations");
   await tx.wait();
   console.log(`Transaction completed. Hash: ${tx.hash}`);
 }
 
+
+
+
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+    console.error(error);
+    process.exitCode = 1;
+  });
